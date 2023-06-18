9600ae62b90be8fb9375bf306b50c9ac2c54196d
landonKHJ <koschino@gmail.com>
2023-06-19 03:41:32 +0900

    package swift name IDNA 로 변경해보기

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "IDNA-Cocoa",
+    name: "IDNA",
 	platforms: [.macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
