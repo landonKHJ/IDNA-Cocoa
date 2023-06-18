ecb95197f792f22f3732b5dfabef84636b5e3735
landonKHJ <koschino@gmail.com>
2023-06-19 03:51:35 +0900

    package 변경해봄

--- Package.swift
+++ Package.swift
@@ -1,31 +1,19 @@
-// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "IDNA",
-	platforms: [.macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "IDNA",
-            targets: ["IDNA"])
+    name: "IDNA-Cocoa",
+    platforms: [
+        .iOS("9.0")
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    products: [
+        .library(name: "IDNA", targets: ["IDNA"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "IDNA",
-			dependencies: [],
-			resources: [.copy("uts46")],
-			swiftSettings: [.define("SWIFT_PACKAGE")]),
-        .testTarget(
-            name: "IDNATests",
-            dependencies: ["IDNA"])
+            path: "IDNA",
+            resources: [.copy("uts46")]
+        )
     ]
 )
